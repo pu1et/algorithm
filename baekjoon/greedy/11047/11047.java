fc10559a35059f895fe7e8e8c81dab38086a46e0
Jiwon An <pu1etproof@gmail.com>
2021-05-30 07:45:06 +0900

    Update 11047.java

--- baekjoon/greedy/11047/11047.java
+++ baekjoon/greedy/11047/11047.java
@@ -5,6 +5,10 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.IOException;
 
+/*
+1. K를 동전의 높은 가치의 개수부터 더함
+2. 높은 가치를 나눈 몫을 빼주며 K 업데이트
+*/
 public class Main{
 
   public static void main(String []args) throws IOException{
@@ -20,9 +24,9 @@ public class Main{
     }
     
     int result = 0;
-    for(int i=N-1;i>=0;i--){
-      result += K/money[i];
-      K%=money[i];
+    for(int i=N-1;i>=0;i--){ // 높은 가치의 개수부터 낮은 가치 순으로
+      result += K/money[i]; // 해당 가치의 개수
+      K%=money[i]; // K 업데이트
       if(K==0) break;
     }
     
