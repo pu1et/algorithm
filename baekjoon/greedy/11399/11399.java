0164b1dac68cc1ebf3a2773d6f3bd8dd014b5818
Jiwon An <pu1etproof@gmail.com>
2021-05-30 07:41:22 +0900

    Update 11399.java

--- baekjoon/greedy/11399/11399.java
+++ baekjoon/greedy/11399/11399.java
@@ -3,6 +3,10 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
+/*
+돈을 인출할 때 걸리는 시간이 이전 사람의 시간들의 누적값이 더해지므로
+오름차순으로 정렬하여 각 인출시간을 더해주면 최소값이 된다.
+*/
 public class Main{
 
   public static void main(String []args) throws IOException{
@@ -17,10 +21,10 @@ public class Main{
       num[i] = Integer.parseInt(tmp[i]);
     }
     
-    Arrays.sort(num);
+    Arrays.sort(num); // 인출 시간 오름차순 정렬
     int sum = 0, block=0;
     
-    for(int i:num){
+    for(int i:num){ // 각 인출 시간을 더함
       sum += (block+i);
       block +=i;
     }
