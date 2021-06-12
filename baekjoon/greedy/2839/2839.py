81662fcd83d5a068f1049075889287e830800638
Jiwon An <pu1etproof@gmail.com>
2021-06-13 07:02:12 +0900

    Update 2839.py

--- baekjoon/greedy/2839/2839.py
+++ baekjoon/greedy/2839/2839.py
@@ -1,12 +1,18 @@
 import sys
 
+'''
+3의 몫을 0 ~ N//3 까지 늘려가며 N%3이 5의 배수면 
+3과 5의 각 몫을 넣어주고 for문을 빠져나온다.
+-> i값이 증가할수록 결과값이 커지므로
+'''
+
 N = int(sys.stdin.readline()[:-1])
 
 result = -1
 for i in range(N//3+1):
-  tmp = N - 3*i
-  if tmp%5 ==0 :
-    result = i + tmp//5
+  tmp = N - 3*i # == N%3
+  if tmp%5 ==0 : # 최소 가방 수 이므로 break
+    result = i + tmp//5 
     break
 
 print(result)
