ee622d6bb8b3a8f6de20acc6dc0cd6c68cb2594a
Jiwon An <pu1etproof@gmail.com>
2021-06-13 08:06:00 +0900

    Update 1080.java

--- baekjoon/greedy/1080/1080.java
+++ baekjoon/greedy/1080/1080.java
@@ -2,9 +2,15 @@ import java.util.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+/*
+1. 행과 열을 증가시키며 행렬 B와 다른 요소가 있으면 3*3 단위로 0은 1로, 1은 0으로 교체해준다.
+2. 결과적으로, 행렬 A와 B가 같지 않으면 -1을 출력한다.
+*/
+
 public class Main{
   static int[][] origin;
 
+  // 3*3 크기의 부분 행렬에 있는 모든 원소를 뒤집음
   public static void change(int row, int col){
     for(int i=row;i<row+3;i++){
       for(int j=col;j<col+3;j++){
@@ -38,6 +44,7 @@ public class Main{
         }
     }
 
+    // 행과 열을 증가시키며 행렬 B와 다른 요소가 있으면 교체한 후, 연산 횟수 1 증가
     for(int i=0;i<N-2;i++){
       for(int j=0;j<M-2;j++){
         if(origin[i][j] != goal[i][j]){
@@ -47,6 +54,7 @@ public class Main{
       }
     }
 
+    // 연산 횟수 출력, but 행렬 A와 B가 같지 않으면 -1 출력
     for(int i=0;i<N;i++){
       for(int j=0;j<M;j++){
         if(origin[i][j] != goal[i][j]){
