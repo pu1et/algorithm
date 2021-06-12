510d2b958cc55ded118aef4dc342f90974be2933
Jiwon An <pu1etproof@gmail.com>
2021-06-13 08:51:25 +0900

    Update 16953.py

--- baekjoon/greedy/16953/16953.py
+++ baekjoon/greedy/16953/16953.py
@@ -1,16 +1,27 @@
 import sys
 
+'''
+B에서 A로 바꾸는 연산을 한다.
+
+1. 끝에 1이 있으면 잘라낸다.
+2. 2의 배수면 나눈다.
+3. 그 외 홀수는 2로 나누어떨어질 수 없기 때문에 -1출력
+'''
+
 A, B = map(int,sys.stdin.readline().split())
 
 result = 1
 while B > A:
+  # 끝에 1 있으면 자름
   tmp = str(B)
   if tmp[-1] == '1':
     B = int(tmp[:-1])
     result += 1
+  # 2의 배수면 나눔
   elif B%2 == 0: 
     B //= 2
     result += 1
+  # 그 외 홀수는 2로 나누어떨어질 수 없기 때문에 -1 출력
   else: break
 
 print(result) if B == A else print(-1)
