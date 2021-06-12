bb6b21faff2cbb7e64ee6194aa52f0428c2a1150
Jiwon An <pu1etproof@gmail.com>
2021-06-13 08:13:29 +0900

    Update 1439.java

--- baekjoon/greedy/1439/1439.java
+++ baekjoon/greedy/1439/1439.java
@@ -2,18 +2,15 @@ import java.util.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+/*
+1과 0중 연속된 수를 바꿔야 하는 횟수가 더 적은 숫자를 뒤집으면 된다.
+
+1. [0 연속 횟수, 1 연속 횟수] 배열을 생성한다.
+2. 둘 중 더 작은 값을 출력한다.
+*/
+
 public class Main{
-  static int[][] origin;
-
-  public static void change(int row, int col){
-    for(int i=row;i<row+3;i++){
-      for(int j=col;j<col+3;j++){
-        if(origin[i][j] == 1) origin[i][j] = 0;
-        else origin[i][j] = 1;
-      }
-    }
-  }
-
+  
   public static void main(String []args) throws Exception{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -22,6 +19,7 @@ public class Main{
     int flag = Integer.parseInt(tmp[0]);
     ++arr[flag];
 
+    // {0 연속 횟수, 1 연속 횟수} 배열 생성
     for(int i=1;i<tmp.length;i++){
       int comp = Integer.parseInt(tmp[i]);
       if(flag != comp){
@@ -29,6 +27,7 @@ public class Main{
         ++arr[flag]; 
       }
     }
+    // 둘 중 더 작은 값 출력
     System.out.println(Math.min(arr[0],arr[1]));
   } 
 }
