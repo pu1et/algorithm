2ea50ef9cb2d72b43d7e5e3c822860029d8a511a
Jiwon An <pu1etproof@gmail.com>
2021-06-13 06:34:07 +0900

    Update 1449.py

--- baekjoon/greedy/1449/1449.py
+++ baekjoon/greedy/1449/1449.py
@@ -1,11 +1,21 @@
 import sys
 
+'''
+제일 낮은 위치부터 테이프를 붙여간다.
+
+1. 물이 새는 위치를 오름차순 정렬한다.
+2. 제일 낮은 위치부터 시작하여 테이프 수를 +1씩 해주고
+   테이프의 길이만큼 위치를 증가시킨다. 
+'''
+
 N, L = map(int,sys.stdin.readline().split())
 thing = list(map(int, sys.stdin.readline().split()))
-thing.sort()
-result = 0
+result, idx = 0, 0
 
-idx = 0
+# 물이 새는 위치 오름차순 정렬
+thing.sort()
+
+# 제일 낮은 위치부터 테이프를 붙여감
 for t in thing:
   if idx < t:
     result += 1
